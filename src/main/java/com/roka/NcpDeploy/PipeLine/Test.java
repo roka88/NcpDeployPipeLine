bab1532490ed439afdfe02fe4f2d36d912869b6f
roka88 <the602@naver.com>
2017-11-19 18:44:22 +0900

    NcpDeploy 0.0.2 readme update

--- src/main/java/com/roka/NcpDeploy/PipeLine/Test.java
+++ src/main/java/com/roka/NcpDeploy/PipeLine/Test.java
@@ -1,10 +1,9 @@
 package com.roka.NcpDeploy.PipeLine;
 
-import com.roka.NcpDeploy.OtherManager.StopJobManager;
+
 
 public class Test {
     public void test() {
-        OperationPipeLine operationPipeLine = new OperationPipeLine();
-        operationPipeLine.setPriorityJob(new StopJobManager().stop());
+
     }
 }
